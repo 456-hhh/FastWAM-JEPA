0ab8aa7c27c3537bbe88d438a973b57875753b0d
456-hhh <347853728@qq.com>
2026-06-01 19:13:55 +0800

    Export V-JEPA modules

--- src/fastwam/models/vjepa/__init__.py
+++ src/fastwam/models/vjepa/__init__.py
@@ -1,3 +1,4 @@
+from .vjepa_ac_joint_predictor import VJepaACJointPredictor
 from .vjepa_encoder_wrapper import VJepaEncoderWrapper
 
-__all__ = ["VJepaEncoderWrapper"]
+__all__ = ["VJepaACJointPredictor", "VJepaEncoderWrapper"]
